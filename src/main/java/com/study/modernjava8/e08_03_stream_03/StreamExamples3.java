c654cf08d1abb722f48c42c94dd0c2fb8a9af8d8
sskim <tjdtjq91@gmail.com>
2021-03-13 19:19:28 +0900

    Feat: 모던 자바8 못다한 이야기 08_03_03 Stream API - 기초

---
+++ src/main/java/com/study/modernjava8/e08_03_stream_03/StreamExamples3.java
@@ -0,0 +1,124 @@
+package com.study.modernjava8.e08_03_stream_03;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * @author sskim
+ */
+public class StreamExamples3 {
+    public static void main(String[] args) {
+        System.out.println("collect(toList()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(toList())
+        );
+
+        System.out.println("collect(toSet()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(toSet())
+        );
+
+        System.out.println("collect(joining()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining())
+        );
+
+        System.out.println("collect(joining(\", \")): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining(", "))
+        );
+
+        System.out.println("collect(joining(\", \", \"[\", \"]\")): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining(", ", "[", "]")) // [#6, #6, #10, #10]
+        );
+
+        System.out.println("distinct().collect(joining(\", \", \"[\", \"]\")): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .distinct()
+                        .collect(joining(", ", "[", "]"))
+        );
+
+        System.out.println("distinct().collect(toList()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .distinct()
+                        .collect(toList())
+        );
+
+        final Integer integer3 = 3;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5)
+                        .filter(i -> i == integer3)
+                        .findFirst()
+        );
+
+        final Integer integer127 = 127;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5, 127)
+                        .filter(i -> i == integer127)
+                        .findFirst()
+        );
+
+        /**
+         *  Equality가 아닌 Identity를 확인
+         *  Equality: equals()를 통해확인
+         *  Identity: ==를 이용해 메모리 레퍼런스 확인
+         *  Integer는 -128~127까지 캐싱을 사용. (Integer클래스 986라인부터 확인)
+         *  그래서 == 127은 같은것으로 간주하지만 128부터는 다른 주소값을 비교하기때문에 false가 된다.
+         */
+        final Integer integer128 = 128;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5, 128)
+                        .filter(i -> i == integer128)
+                        .findFirst()
+        );
+
+
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5, 128)
+                        .filter(i -> i.equals(integer128))
+                        .findFirst()
+        );
+
+        System.out.println(".filter(i -> i > integer3).count(): " +
+                Stream.of(1, 2, 3, 4, 5)
+                        .filter(i -> i > integer3)
+                        .count()
+        );
+
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println("for (Integer i : numbers): ");
+        for (Integer i : numbers) {
+            System.out.print("i = " + i + " ");
+        }
+
+        System.out.println("\n\nforEach(i -> System.out.println(i)): ");
+        Stream.of(1, 2, 3, 4, 5)
+                .forEach(i -> System.out.print(i + " "));
+
+    }
+}
